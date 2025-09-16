99fc6a8e9ee6d14d79e78eb1f28abf092389a0ad
YouOnlyLiveOnce124 <semendimakov92@gmail.com>
2025-09-16 22:48:33 +0200

    "fix: replace BrowserRouter with HashRouter for GitHub Pages SPA compatibility
    - Changed from BrowserRouter to HashRouter to prevent 404 errors on direct URL access
    - Added ErrorBoundary component to catch and handle React errors gracefully
    - Ensured proper routing functionality on GitHub Pages hosting
    - Maintained Redux Provider and StrictMode wrappers"

--- src/index.js
+++ src/index.js
@@ -3,17 +3,20 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import mainStore from "./reducer/reduxStore";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import ErrorBoundary from "./ErrorBoundary";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={mainStore}>
-        <App />
+        <ErrorBoundary>
+          <App />
+        </ErrorBoundary>
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
